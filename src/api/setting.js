a6c7f733ce40f2f795ef075a6dfbe3fe1e8c46a9
AYuWei <1422041216@qq.com>
2022-08-06 11:27:43 +0800

    页面使用vuex

---
+++ src/api/setting.js
@@ -0,0 +1,8 @@
+/**
+ * 获取全局设置
+ */
+import request from "./request";
+
+export async function getSetting() {
+  return await request.get("/api/setting");
+}
---
+++ src/mock/setting.js
@@ -0,0 +1,18 @@
+import Mock from "mockjs";
+Mock.mock("/api/setting", "get", {
+  code: 0,
+  msg: "",
+  data: {
+    avatar: "https://img2.baidu.com/it/u=4244269751,4000533845&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500",
+    siteTitle: " Vue2_小小青柠蓝莓 ",
+    github: "https://github.com/AYuWei",
+    qq: "1422041216",
+    qqQrCode:
+      "",
+    weixin: "AYuWei0804",
+    weixinQrCode:
+      "",
+    mail: "ayuwei0804@126.com", 
+    githubName: "AYuWei", 
+  },
+});
